1beed9ff24f884b6be1700529aaeba639c332cb5
Tim Akinbo <takinbo@timbaobjects.com>
2016-11-22 02:08:17 +0100

    if there's a problem fetching the quotes, use 0 instead

--- exchanges.py
+++ exchanges.py
@@ -18,10 +18,13 @@ class BitX(Exchange):
     symbol = 'bitx'
 
     def exchange_rate(self):
-        res = requests.get("https://api.mybitx.com/api/1/tickers").json()
-        if res:
-            tradepair = next(filter(lambda t: t['pair'] == 'XBTNGN', res['tickers']))
-            return Decimal(tradepair['last_trade'])
+        try:
+            res = requests.get("https://api.mybitx.com/api/1/tickers").json()
+            if res:
+                tradepair = next(filter(lambda t: t['pair'] == 'XBTNGN', res['tickers']))
+                return Decimal(tradepair['last_trade'])
+        except:
+            return Decimal('0')
 
 
 class LBC(Exchange):
@@ -29,9 +32,12 @@ class LBC(Exchange):
     symbol = 'lb'
 
     def exchange_rate(self):
-        res = requests.get("https://localbitcoins.com/bitcoinaverage/ticker-all-currencies/").json()
-        if res:
-            return Decimal(res['NGN']['rates']['last'])
+        try:
+            res = requests.get("https://localbitcoins.com/bitcoinaverage/ticker-all-currencies/").json()
+            if res:
+                return Decimal(res['NGN']['rates']['last'])
+        except:
+            return Decimal('0')
 
 
 __exchanges__ = [BitX, LBC]
