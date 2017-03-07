b67dd2b1e7863ddd1ec5419be2be1c8580d2b688
Tim Akinbo <takinbo@timbaobjects.com>
2017-03-07 02:58:58 +0100

    added support for the webhook

--- bot.py
+++ bot.py
@@ -6,16 +6,21 @@ from prettyconf import config
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.DEBUG)
+updater = Updater(config('TOKEN'))
+
+updater.dispatcher.add_handler(CommandHandler('start', start_command))
+updater.dispatcher.add_handler(CommandHandler('market', market_command))
+updater.dispatcher.add_handler(CommandHandler('price', price_command, pass_args=True))
+updater.dispatcher.add_handler(CommandHandler('calc', calc_command, pass_args=True))
+updater.dispatcher.add_handler(CommandHandler('help', help_command, pass_args=True))
+updater.dispatcher.add_handler(MessageHandler(Filters.command, unknown_command))
+
+prod = config('PRODUCTION', default=False, cast=config.boolean)
+port = config('PORT', default="8001", cast=config.eval)
 
 if __name__ == '__main__':
-    updater = Updater(config("TOKEN"))
-
-    updater.dispatcher.add_handler(CommandHandler('start', start_command))
-    updater.dispatcher.add_handler(CommandHandler('market', market_command))
-    updater.dispatcher.add_handler(CommandHandler('price', price_command, pass_args=True))
-    updater.dispatcher.add_handler(CommandHandler('calc', calc_command, pass_args=True))
-    updater.dispatcher.add_handler(CommandHandler('help', help_command, pass_args=True))
-    updater.dispatcher.add_handler(MessageHandler(Filters.command, unknown_command))
-
-    updater.start_polling()
+    if prod:
+        updater.start_webhook(port=port, url_path=config('TOKEN'))
+    else:
+        updater.start_polling()
     updater.idle()
